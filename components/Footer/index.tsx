ac54dd8d60843b3746eb54829fe0ae126f5069b8
Href <href.indonesia@gmail.com>
2022-08-23 22:18:05 +0700

    responsive footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,73 +7,71 @@ import TextureImg from "../../asset/pictures/paper-texture-3.png";
 const Footer = () => {
   return (
     <footer
-      className="w-screen bg-darkRed py-16 flex justify-center"
+      className="flex w-screen justify-center bg-darkRed py-16"
       style={{
         backgroundImage: `url(${TextureImg.src})`,
         backgroundSize: "100%",
         backgroundBlendMode: "multiply",
       }}
     >
-      <div className="flex justify-between items-center w-3/5 xl:w-1/2 text-white gap-32">
-        <div className="flex-1 flex justify-center items-center">
-          <LogoLightSVG className="w-full max-w-[13rem] min-w-[5rem]" />
+      <div className="flex w-full flex-col justify-between gap-10 px-10 text-center text-white md:flex-row md:gap-0 lg:w-4/5 lg:px-0 xl:w-3/5">
+        <div className="flex items-center justify-center">
+          <LogoLightSVG className="w-32" />
         </div>
-        <div className="flex flex-[4] gap-10 justify-between">
-          <div className="flex flex-col gap-2">
-            <span className="bg-skinCream max-w-content px-3 py-1 font-black text-lg text-black mb-2">
-              QUICK LINKS
-            </span>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                Product Catalog
-              </a>
-            </Link>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                Our Materials
-              </a>
-            </Link>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                Become a Partner
-              </a>
-            </Link>
-          </div>
-          <div className="flex flex-col gap-2 ">
-            <p className="bg-skinCream inline-block px-3 py-1 font-black text-lg text-black mb-2">
-              MARKETPLACE
-            </p>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                Tokopedia
-              </a>
-            </Link>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                Shopee
-              </a>
-            </Link>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p className="bg-skinCream inline-block px-3 py-1 font-black text-lg text-black mb-2">
-              ABOUT
-            </p>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                Our Journey
-              </a>
-            </Link>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                Blog
-              </a>
-            </Link>
-            <Link href={"/"}>
-              <a className="hover:text-paleGreen transition-all ease-out duration-200">
-                FAQs
-              </a>
-            </Link>
-          </div>
+        <div className="flex flex-col gap-2">
+          <span className="max-w-content mb-2 bg-skinCream px-3 py-1 text-lg font-black text-black">
+            QUICK LINKS
+          </span>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              Product Catalog
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              Our Materials
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              Become a Partner
+            </a>
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2 ">
+          <p className="mb-2 inline-block bg-skinCream px-3 py-1 text-lg font-black text-black">
+            MARKETPLACE
+          </p>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              Tokopedia
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              Shopee
+            </a>
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="mb-2 inline-block bg-skinCream px-3 py-1 text-lg font-black text-black">
+            ABOUT
+          </p>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              Our Journey
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              Blog
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a className="transition-all duration-200 ease-out hover:text-paleGreen">
+              FAQs
+            </a>
+          </Link>
         </div>
       </div>
     </footer>
