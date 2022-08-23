dc85e4a75bc47e3f569c379fad6144006527ccc7
Href <href.indonesia@gmail.com>
2022-08-23 10:27:09 +0700

    added seo and fixed navbar

---
+++ next-seo.config.js
@@ -0,0 +1,8 @@
+const seo = {
+  titleTemplate: "ECOPLEASE | %s",
+  defaultTitle: "ECOPLEASE | The Best Eco-Packaging in Indonesia",
+  description: "The best eco-friendly and sustainable packaging",
+  canonical: "https://www.canonical.ie/",
+};
+
+export default seo;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
+
+import SEO from "../next-seo.config";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="overflow-x-hidden">
-      <Header />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <DefaultSeo {...SEO} />
+      <div className="overflow-x-hidden">
+        <Header />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
