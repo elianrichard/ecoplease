5e74cb805aea43593c9f58be7ec53b6b9a838c66
Href <href.indonesia@gmail.com>
2022-08-23 21:49:42 +0700

    response partners

--- modules/HomePage/JoinedContent.tsx
+++ modules/HomePage/JoinedContent.tsx
@@ -4,12 +4,15 @@ import paperTextureImg from "../../asset/pictures/paper-texture-2.jpg";
 
 const JoinedContent = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-screen mb-1 py-20 bg-skinCream/80 flex flex-col items-center gap-20" style={{
+    <div
+      className="mb-1 flex w-screen flex-col items-center gap-20 bg-skinCream/80 py-20"
+      style={{
         backgroundImage: `url("${paperTextureImg.src}")`,
         backgroundSize: "contain",
         backgroundPosition: "top center",
         backgroundBlendMode: "multiply",
-      }}>
+      }}
+    >
       {children}
     </div>
   );
--- modules/HomePage/Partners.tsx
+++ modules/HomePage/Partners.tsx
@@ -4,18 +4,21 @@ import KopiKenanganSVG from "../../asset/svgs/kopiKenangan";
 
 const Partners = () => {
   return (
-    <div className="flex flex-col items-center w-4/5 gap-20">
-      <p className="text-darkRed font-black text-5xl text-center w-full relative before:content-['OUR_PARTNERS'] before:absolute before:top-1 before:translate-x-1 text-outline z-0 before:-z-10">
+    <div className="flex w-full flex-col items-center gap-20">
+      <p className="text-outline relative z-0 w-full text-center text-4xl font-black text-darkRed before:absolute before:top-1 before:-z-10 before:translate-x-1 before:content-['OUR_PARTNERS'] xs:text-5xl">
         OUR PARTNERS
       </p>
-      <div className="flex justify-center gap-12">
+      <div className="flex flex-wrap justify-center gap-6 xs:gap-12">
         <KopiKenanganSVG />
         <KopiKenanganSVG />
         <KopiKenanganSVG />
         <KopiKenanganSVG />
         <KopiKenanganSVG />
       </div>
-      <a href="#" className="bg-darkRed text-white text-xl px-6 py-2 rounded-lg hover:bg-white hover:text-darkRed hover:scale-110 hover:shadow-[0px_0px_20px_0px_rgba(0,0,0,0.5)] transition-all duration-200 ease-out">
+      <a
+        href="#"
+        className="rounded-lg bg-darkRed px-6 py-2 text-lg text-white transition-all duration-200 ease-out hover:scale-110 hover:bg-white hover:text-darkRed hover:shadow-[0px_0px_20px_0px_rgba(0,0,0,0.5)] xs:text-xl"
+      >
         Become Our Partner <span className="font-bold">NOW!</span>
       </a>
     </div>
