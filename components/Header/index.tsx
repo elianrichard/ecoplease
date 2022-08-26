2b7bbe528398574043879007bd1de104f793f130
Href <href.indonesia@gmail.com>
2022-08-26 20:46:42 +0700

    responsive for our story and nav resize fix

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -45,10 +45,10 @@ const Header = () => {
         </IconContext.Provider>
       </div>
       <div
-        className={`absolute top-[100%] left-0 flex w-screen flex-col items-center justify-between gap-10 bg-ecoRed py-10 shadow-[0px_10px_15px_-5px_rgba(0,0,0,0.5)] transition-opacity duration-200 ease-out lg:pointer-events-auto lg:static lg:h-full lg:w-full lg:flex-row lg:bg-transparent lg:py-0 lg:shadow-none ${
+        className={`absolute top-[100%] left-0 flex w-screen flex-col items-center justify-between gap-10 py-10 shadow-[0px_10px_15px_-5px_rgba(0,0,0,0.5)] transition-opacity duration-200 ease-out lg:pointer-events-auto lg:static lg:h-full lg:w-full lg:flex-row lg:bg-transparent lg:py-0 lg:shadow-none lg:transition-none ${
           isNavbarOpen
-            ? "pointer-events-auto opacity-100"
-            : "pointer-events-none opacity-0"
+            ? "pointer-events-auto opacity-100 bg-ecoRed"
+            : "pointer-events-none opacity-0 bg-transparent"
         } lg:opacity-100`}
       >
         <ul className="flex h-full w-full flex-col gap-10 lg:flex-row lg:gap-6 lg:py-0 xl:gap-12">
--- modules/AboutPage/OurStory.tsx
+++ modules/AboutPage/OurStory.tsx
@@ -5,29 +5,29 @@ import OurStoryBgImg from "../../asset/pictures/about/our-story-bg.jpg";
 
 const OurStory = () => {
   return (
-    <div className="flex h-[calc(100vh-64px)] w-screen mb-1">
+    <div className="mb-1 flex w-screen flex-col md:h-[calc(100vh-64px)] md:flex-row">
       <div
-        className="flex h-full flex-[2] items-center justify-center bg-darkRed text-center text-7xl font-bold uppercase text-white"
+        className="flex h-full flex-[2] items-center justify-center bg-darkRed text-center text-4xl md:text-6xl font-bold uppercase leading-tight text-white lg:text-7xl lg:leading-tight"
         style={{
           backgroundImage: `url(${OurStoryBgImg.src})`,
           backgroundSize: "cover",
           backgroundBlendMode: "multiply",
         }}
       >
-        Our Story
+        <p className="px-10 py-10 md:py-0">Our Story</p>
       </div>
       <div
-        className="h-full flex-[3] bg-skinCream"
+        className="h-full flex-[4] bg-skinCream lg:flex-[3]"
         style={{
           backgroundImage: `url(${TextureImg.src})`,
           backgroundSize: "cover",
           backgroundBlendMode: "multiply",
         }}
       >
-        <div className="flex h-full w-full flex-col items-start justify-center gap-5 px-20 text-xl">
-          <p className="text-5xl font-bold uppercase leading-tight">
-            How we end up
-            <br />
+        <div className="flex h-full w-full flex-col items-start justify-center gap-5 px-10 py-10 text-lg md:py-0 lg:px-20 xl:text-xl">
+          <p className="text-4xl font-bold uppercase leading-tight xl:leading-tight xl:text-5xl">
+            How we end up{" "}
+            <br className="hidden md:block"/>
             <span className="bg-darkGreen px-3 text-white">
               caring the world?
             </span>
@@ -39,8 +39,8 @@ const OurStory = () => {
           </p>
           <p>
             We are so used to a
-            <span className="font-bold text-darkRed"> convenient</span>{" "}
-            lifestyle that we often forget to calculate the impact that it
+            <span className="font-bold text-darkRed"> convenient lifestyle</span>{" "}
+            that we often forget to calculate the impact that it
             causes to the environment. Here in
             <span className="font-bold text-darkRed"> Ecoplease</span>, we
             believe that convenience (single use products),{" "}
@@ -49,7 +49,7 @@ const OurStory = () => {
             </span>
           </p>
           <p>
-            We do our research and experiments to find the best eco alternatives
+            We do our research and experiments to find the best eco-alternatives
             for single use plastic products.
           </p>
         </div>
