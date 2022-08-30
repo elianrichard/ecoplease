c3f588e0b9172166f6f858b96f142537b4abece0
Href <href.indonesia@gmail.com>
2022-08-30 13:37:23 +0700

    added responsive for eco news

--- modules/EcoNews/Layout.tsx
+++ modules/EcoNews/Layout.tsx
@@ -55,12 +55,12 @@ const Layout = () => {
     >
       <EconewsRipped />
       <EconewsRippedFlip />
-      <div className="min-h-screen w-full bg-black/30 p-16">
+      <div className="min-h-screen w-full bg-black/30 sm:px-16 py-16 px-10">
         <div className="mb-16 text-center text-skinCream">
-          <p className="before:title-outline-skinCream relative z-0 mb-5 text-5xl font-bold before:absolute before:top-1 before:-z-10 before:translate-x-1 before:content-['ECO_NEWS']">
+          <p className="before:title-outline-skinCream relative z-0 mb-5 text-4xl sm:text-5xl font-bold before:absolute before:top-1 before:-z-10 before:translate-x-1 before:content-['ECO_NEWS']">
             ECO NEWS
           </p>
-          <p className="text-xl">Your daily dose of eco-fiendly news</p>
+          <p className="text-lg sm:text-xl">Your daily dose of eco-fiendly news</p>
         </div>
         <div className="flex flex-col gap-10">
           {newsPlaceholder.map((el, i) => (
--- modules/EcoNews/NewsCard.tsx
+++ modules/EcoNews/NewsCard.tsx
@@ -15,12 +15,14 @@ interface Props {
 const NewsCard = ({ article, index }: Props) => {
   return (
     <div
-      className={`flex h-80 w-4/5 ${
-        (index + 1) % 2 === 0 ? "flex-row-reverse self-end" : "flex-row"
+      className={`min-h-80 flex w-full xl:w-4/5 ${
+        (index + 1) % 2 === 0
+          ? "flex-col self-end sm:flex-row-reverse"
+          : "flex-col sm:flex-row"
       } overflow-hidden rounded-xl bg-skinCream`}
     >
       <div
-        className="relative h-full w-3/5 bg-skinCream before:absolute before:h-full before:w-full before:bg-black/40"
+        className="min-h-80 custom-clip-econews relative h-80 w-full bg-skinCream before:absolute before:h-full before:w-full before:bg-black/40 sm:h-auto sm:w-2/5 md:w-3/5"
         style={{
           clipPath:
             (index + 1) % 2 === 0
@@ -30,15 +32,14 @@ const NewsCard = ({ article, index }: Props) => {
           backgroundSize: "cover",
           backgroundPosition: "center center",
           backgroundBlendMode: "multiply",
-          boxShadow: "0px 0px 88px -40px rgba(0, 0, 0) inset",
         }}
       />
-      <div className="flex h-full w-2/5 translate-x-2 flex-col gap-2 p-10 text-darkRed">
-        <p className="text-3xl font-bold">{article.title}</p>
-        <p className="mb-5 font-bold text-ecoRed">
+      <div className="flex h-full w-full translate-x-2 flex-col gap-2 p-6 sm:p-10 text-darkRed sm:w-3/5 md:w-2/5">
+        <p className="text-2xl font-bold sm:text-3xl">{article.title}</p>
+        <p className="mb-3 sm:mb-5 text-sm font-bold text-ecoRed sm:text-base">
           {article.author} • {article.date}
         </p>
-        <p>{article.excerpt}</p>
+        <p className="text-sm sm:text-base">{article.excerpt}</p>
       </div>
     </div>
   );
