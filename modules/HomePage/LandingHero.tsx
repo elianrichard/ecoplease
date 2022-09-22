c20d52f36b961ce13e3828bf3bccdba9b4d9a909
Href <href.indonesia@gmail.com>
2022-09-22 18:16:14 +0700

    added landing animation

--- modules/HomePage/LandingHero.tsx
+++ modules/HomePage/LandingHero.tsx
@@ -7,8 +7,32 @@ import heroLandingImg from "../../asset/pictures/home/landing-hero.webp";
 
 // import RippedSVG from "../../asset/svgs/ripped/RippedLanding";
 import RippedLandingSVG from "../../asset/svgs/ripped/RippedLandingHome";
+import { AnimatePresence, motion, useAnimationControls } from "framer-motion";
 
 const LandingHero = () => {
+  const containerVar = {
+    hidden: { opacity: 0, x: -100 },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+        delay: 0.2,
+        type: "spring",
+        stiffness: 80,
+      },
+    },
+  };
+
+  const childrenVar = {
+    hidden: { opacity: 0, x: -100 },
+    show: { opacity: 1, x: 0 },
+  };
+
+  const productImageDivControls = useAnimationControls();
+  const productImageControls = useAnimationControls();
+
   return (
     <div
       className={`relative mb-1 flex w-screen justify-center overflow-hidden lg:h-[calc(100vh-64px)]`}
@@ -25,7 +49,7 @@ const LandingHero = () => {
       </div>
       <RippedLandingSVG />
       <div
-        className="absolute top-0 left-0 w-4/5 lg:h-screen "
+        className="absolute top-0 left-0 w-4/5 lg:h-screen"
         style={{ clipPath: `url(#landingHomeMask)` }}
       >
         <Image
@@ -40,7 +64,16 @@ const LandingHero = () => {
         {/* <RippedSVG className="z-20 hidden h-full w-full drop-shadow-[0px_0px_20px_rgba(0,0,0,0.8)] lg:block" /> */}
       </div>
       <div className="z-0 flex w-full flex-col items-center justify-center gap-10 py-32 lg:w-4/5 lg:flex-row lg:py-0">
-        <div className="flex w-full flex-col items-center justify-center lg:items-start">
+        <motion.div
+          variants={containerVar}
+          initial="hidden"
+          animate="show"
+          onAnimationComplete={async () => {
+            await productImageDivControls.start({ opacity: 1, y: 0 });
+            await productImageControls.start({ y: 50 });
+          }}
+          className="flex w-full flex-col items-center justify-center lg:items-start"
+        >
           <h1 className="mb-10 text-center text-4xl font-bold leading-snug text-white sm:text-5xl sm:leading-tight lg:text-left lg:text-6xl lg:leading-snug">
             100% HOME
             <br />
@@ -48,37 +81,57 @@ const LandingHero = () => {
             <br />
             PACKAGING
           </h1>
-          <a
+          <motion.a
+            variants={childrenVar}
             href="#"
             className="clip-path-triangle relative mb-5 bg-skinCream py-2 px-10 text-center text-xl font-bold text-darkRed transition-all duration-200 ease-out hover:px-14 lg:bg-ecoRed lg:text-3xl lg:text-skinCream lg:hover:pl-16 lg:hover:text-white"
           >
             OUR CATALOG
-          </a>
-          <a
+          </motion.a>
+          <motion.a
+            variants={childrenVar}
             href="#"
             className="clip-path-triangle relative mb-5 bg-skinCream py-2 px-10 text-center text-xl font-bold text-darkRed transition-all duration-200 ease-out hover:px-14 lg:bg-ecoRed lg:text-3xl lg:text-skinCream lg:hover:pl-16 lg:hover:text-white"
           >
             COSTUMIZE PRODUCT
-          </a>
-        </div>
-        <div className="relative flex h-96 w-full items-center justify-center lg:h-4/5">
-          <Image
-            src={productLandingImg}
-            layout="fill"
-            objectFit="contain"
-            alt="ECOPLEASE product"
-            style={{ filter: "brightness(90%)" }}
-            className="lg:object-right-[10px] object-center"
-            priority
-          />
-        </div>
+          </motion.a>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={productImageDivControls}
+          className="relative flex h-96 w-full items-center justify-center lg:h-4/5"
+        >
+          <motion.div
+            initial={{ y: 10 }}
+            transition={{
+              repeatType: "mirror",
+              repeat: Infinity,
+              duration: 2,
+            }}
+            animate={productImageControls}
+            className="relative flex h-full w-full items-center justify-center"
+          >
+            <Image
+              src={productLandingImg}
+              layout="fill"
+              objectFit="contain"
+              alt="ECOPLEASE product"
+              style={{ filter: "brightness(90%)" }}
+              className="lg:object-right-[10px] object-center"
+              priority
+            />
+          </motion.div>
+        </motion.div>
       </div>
-      <a
+      <motion.a
+        initial={{ x: 400 }}
+        animate={{ x: 0 }}
+        transition={{ type: "spring", stiffness: 80 }}
         href="#"
         className="absolute top-0 right-0 z-20 flex w-full items-center justify-center bg-black py-5 pl-5 pr-5 text-center text-xl font-bold text-skinCream shadow-[0px_0px_20px_0px_rgba(0,0,0,0.8)] lg:top-10 lg:w-auto lg:justify-start lg:rounded-l-lg lg:py-1 lg:pr-20"
       >
         GET YOUR FREE SAMPLE NOW!
-      </a>
+      </motion.a>
     </div>
   );
 };
